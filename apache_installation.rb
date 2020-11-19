326adc42e5d2f9a00b1bf3526fd63736b11df55e
Roshan Munavar <roshan.ahtina@gmail.com>
2020-11-19 14:11:37 +0530

    Update apache_installation.rb

--- apache_installation.rb
+++ apache_installation.rb
@@ -1,15 +1,15 @@
 # This is a simple file to install a HTTPD and Create a index.html file
 package 'httpd' do
-  action  :install
+  action  :remove
 end
 
 # Create a index.html file and put some hello world content in it...
 file '/var/www/html/index.html' do
   content '<html>This is a file to say HELLO WORLD !!!!</html>'
-  action :create
+  action :delete
 end
 
 # HTTPD server is to install
 service 'httpd' do
-  action :start
+  action :nothing
 end
