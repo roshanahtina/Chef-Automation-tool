400a110e2eed16b87f79bdc387aca26603f21c3d
Roshan Munavar <roshan.ahtina@gmail.com>
2020-11-20 08:54:38 +0530

    Update FIle_and_directory_create.rb

--- Basic_Resource_file_Ex/FIle_and_directory_create.rb
+++ Basic_Resource_file_Ex/FIle_and_directory_create.rb
@@ -1,13 +1,13 @@
 directory '/home/ansible/chef_testdir' do
   owner 'cloud_user'
-  group 'cloud_user'
+  group 'ansible'
   mode '666'
   action :create
 end
 
 file '/home/ansbile/chef_testdir/chef_testfile' do
   owner 'cloud_user'
-  group 'cloud_user'
+  group 'ansible'
   mode '444'
   action :create
   content "This is a test file to make entires in chef_testfile\n"
