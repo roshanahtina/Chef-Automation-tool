0e29d020ab5382b0ef20290db6fa1369bde5cbe8
Roshan Munavar <roshan.ahtina@gmail.com>
2020-11-20 08:57:48 +0530

    Update FIle_and_directory_create.rb

--- Basic_Resource_file_Ex/FIle_and_directory_create.rb
+++ Basic_Resource_file_Ex/FIle_and_directory_create.rb
@@ -1,12 +1,12 @@
 directory '/home/ansible/chef_testdir' do
-  owner 'cloud_user'
+  owner 'ansible'
   group 'ansible'
   mode '666'
   action :create
 end
 
 file '/home/ansbile/chef_testdir/chef_testfile' do
-  owner 'cloud_user'
+  owner 'ansible'
   group 'ansible'
   mode '444'
   action :create
