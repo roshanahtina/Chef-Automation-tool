1756efd3e66d62d38ba0458a71a4910031d05326
Roshan Munavar <roshan.ahtina@gmail.com>
2020-11-22 11:41:47 +0530

    Update default.rb

--- cookbooks/webserver/recipes/default.rb
+++ cookbooks/webserver/recipes/default.rb
@@ -3,8 +3,8 @@
 # Recipe:: default
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
-include_recipe 'webserver::apacheindex'
 include_recipe 'webserver::apache'
+include_recipe 'webserver::apacheindex'
 include_recipe 'webserver::apacheservice'
 include_recipe 'ntpserver::ntpserviceins'
 include_recipe 'ntpserver::ntpservice'
