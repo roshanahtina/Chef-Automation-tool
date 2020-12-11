2135ba65f8690459d1c7caeaa1fab64d6e462fbf
Roshan Munavar <roshan.ahtina@gmail.com>
2020-12-11 14:30:14 +0530

    Update FIle_and_directory_create.rb

--- Basic_Resource_file_Ex/FIle_and_directory_create.rb
+++ Basic_Resource_file_Ex/FIle_and_directory_create.rb
@@ -13,3 +13,12 @@ file '/home/ansible/chef_testdir/chef_testfile' do
   action :touch
   content "This is a test file to make entires in chef_testfile\n"
 end
+
+node['ipaddress']
+node['hostname']
+node['memory']['total']
+
+file '/home/cloud_user/ipaddres_hostname_details' do
+  content "This is a file with IPADDRESS Details #{node['ipaddress']} \nAND hostname is #{node['hostname']} \nAND Total Memory #{node['memory']['total']}\n"
+end
+
